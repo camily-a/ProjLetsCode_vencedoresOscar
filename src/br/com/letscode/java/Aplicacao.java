a3a7217543122d5e5c0cf5869b562eb115b351fd
sti <sti@matera.com>
2022-03-10 15:14:11 -0300

    att

--- src/br/com/letscode/java/Aplicacao.java
+++ src/br/com/letscode/java/Aplicacao.java
@@ -1,12 +1,8 @@
 package br.com.letscode.java;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.Collections.reverseOrder;
 
@@ -21,42 +17,15 @@ public class Aplicacao {
         app.atoresEAtrizesQueReceberamMaisDeUmOscar();
         app.buscaAtorAtriz("Katharine Hepburn");
         app.buscaAtorAtriz("Tom Hanks");
-
+        app.buscaAtorAtriz("Thomas Heilord");
 
 
     }
-
-    List<Oscar> lista = new ArrayList<>();
-
-    List<Oscar> listaCompleta = new ArrayList<>();
-
-
-    private List<Oscar> prepararLeituraArquivoCsv( String path) {
-        try (Stream<String> lines = Files.lines(Path.of(path))) {
-            this.lista = lines.skip(1)
-                    .map(Oscar::fromLine)
-                    .collect(Collectors.toList());
-
-            return this.lista;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    private void listaCompleta() {
-        if (listaCompleta.isEmpty()) {
-            listaCompleta.addAll(prepararLeituraArquivoCsv("files/oscar_age_male.csv"));
-            listaCompleta.addAll(prepararLeituraArquivoCsv("files/oscar_age_female.csv"));
-        }
-    }
-
 
     public void atorMaisJovemAGanharOscar() {
-        prepararLeituraArquivoCsv("files/oscar_age_male.csv");
+        LeituraArquivo la = new LeituraArquivo("files/oscar_age_male.csv");
         System.out.print("O ator mais jovem a ganhar o Oscar é ");
-        this.lista.stream()
+        la.getlista().stream()
                 .min(Comparator.comparingInt(Oscar::getIdade))
                 .ifPresent(x -> System.out.println(x.getNome() + " que recebeu o prêmio em " + x.getAno() +
                         " aos " + x.getIdade() + " anos de idade , por sua atuação no filme " + x.getFilme() + ".\n"));
@@ -64,56 +33,58 @@ public class Aplicacao {
     }
 
     public void atrizMaisPremiada() {
-        this.prepararLeituraArquivoCsv("files/oscar_age_female.csv");
+        LeituraArquivo la = new LeituraArquivo("files/oscar_age_female.csv");
         System.out.print("A atriz mais premiada é ");
-        Map<String, Long> numeroPremiacoes = this.lista.stream()
+        la.getlista().stream()
                 .map(Oscar::getNome)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        numeroPremiacoes.entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
                 .max(Comparator.comparingLong(Map.Entry::getValue))
                 .ifPresent(x -> System.out.println(x.getKey() + " com um total de " + x.getValue() + " premiações. \n"));
     }
 
     public void atrizEntre20e30maisPremiada() {
-        this.prepararLeituraArquivoCsv("files/oscar_age_female.csv");
+        LeituraArquivo la = new LeituraArquivo("files/oscar_age_female.csv");
         System.out.print("A atriz com mais premiações entre 20 e 30 anos é ");
-        Map<String, Long> numeroPremiacoes = this.lista.stream()
+        la.getlista().stream()
                 .filter( atriz -> atriz.getIdade() >= 20 && atriz.getIdade() <= 30)
                 .map(Oscar::getNome)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        numeroPremiacoes.entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
                 .max(Comparator.comparingLong(Map.Entry::getValue))
                 .ifPresent(x -> System.out.println(x.getKey() + " com um total de " + x.getValue() + " premiações. \n "));
     }
 
     public void atoresEAtrizesQueReceberamMaisDeUmOscar() {
-        listaCompleta();
-        System.out.print("Os atores ou atrizes que receberam mais de um Oscar são \n");
-        var numeroPremiacoes = this.listaCompleta.stream()
+        LeituraArquivo la = new LeituraArquivo("listaCompleta");
+        System.out.print("Os atores ou atrizes que receberam mais de um Oscar são: \n");
+        var NomesEqtdPremiacoes = la.getlista().stream()
                 .map(Oscar::getNome)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        var NomesQtdPremiacoes = numeroPremiacoes.entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
                 .filter(x -> x.getValue() >= 2)
                 .sorted(reverseOrder(Map.Entry.comparingByValue()))
                 .collect(Collectors.toList());
 
-        NomesQtdPremiacoes.forEach(x -> System.out.println(x.getKey() + " = " + x.getValue() + " Oscars."));
+        NomesEqtdPremiacoes.forEach(x -> System.out.println(x.getKey() + " = " + x.getValue() + " Oscars."));
         System.out.println("\n");
     }
 
     public void buscaAtorAtriz(String nome) {
-        listaCompleta();
-        var resBusca = listaCompleta.stream()
-        .filter( linha -> linha.getNome().equals(nome))
-        .sorted(Comparator.comparingInt(Oscar::getAno))
-        .collect(Collectors.toList());
+        LeituraArquivo la = new LeituraArquivo("listaCompleta");
+        var resBusca = la.getlista().stream()
+                .filter( linha -> linha.getNome().equals(nome))
+                .sorted(Comparator.comparingInt(Oscar::getAno))
+                .collect(Collectors.toList());
 
-        System.out.print(nome.toUpperCase() + " foi premiado(a) " + resBusca.size() + " vezes : \n");
-        resBusca.forEach(x -> System.out.println("Em " + x.getAno() +
-                ", aos " + x.getIdade() + " anos de idade por sua atuação no filme '" + x.getFilme()+ "'"));
-        System.out.print("\n");
-
-        // optional??
+        if (resBusca.isEmpty()) {
+            System.out.print("Não foi encontrado nenhum prêmio associado a " + nome.toUpperCase() + ".\n");
+        } else {
+            System.out.print(nome.toUpperCase() + " foi premiado(a) " + resBusca.size() + " vezes : \n");
+            resBusca.forEach(x -> System.out.println("Em " + x.getAno() +
+                    ", aos " + x.getIdade() + " anos de idade por sua atuação no filme '" + x.getFilme()+ "'"));
+            System.out.print("\n");
+        }
 
     }
 
